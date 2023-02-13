a3679485d2e6ea03735e56d88b03ada6535918ea
PinoEdu <eduardo.pinoh@usm.cl>
2023-02-13 17:00:58 -0300

    Commit 004: Funciones

---
+++ 3.js
@@ -0,0 +1,17 @@
+function mostrarHolaMundo(){
+    console.log("Hola mundo");
+}
+
+function sumar(a,b){
+    var suma = a + b;
+    console.log("El valor de la suma es " + suma);
+}
+
+sumar(15,6);
+
+function resta(a,b){
+    return a - b;
+}
+
+console.log(resta(5,6));
+
