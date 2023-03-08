29f1b345ea62bef4989c6560182a65ec6a41bb39
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-07 22:52:58 -0300

    feat: operadores y condicionales

--- main/tipos_de_operadores.js
+++ main/tipos_de_operadores.js
@@ -23,3 +23,55 @@ i *= 3;
 // Operadores unarios
 i++;
 i--;
+
+// ***Condicionales***
+let edad = 23;
+
+if (edad >= 18){
+    console.log("Eres mayor de edad");
+} else{
+    console.log("Eres menor de edad");
+}
+
+let hora = 5;
+if (0 < hora < 6){
+    console.log("Dejaem dormir");
+} else if (5 < hora < 12){
+    console.log("Buenos días");
+} else if (11 < hora < 19) {
+    console.log("Buenas tardes");
+} else{
+    console.log("Buenas noches");
+}
+
+// Operadores ternarios
+let eresMayor = (edad >= 18) ? console.log("Eres mayor de edad") : console.log("No eres mayor de edad");
+
+// Switch
+let dia = 2;
+
+switch(dia){
+    case 0:
+        console.log("Domingo");
+        break
+    case 2:
+        console.log("Lunes");
+        break
+    case 2:
+        console.log("Martes");
+        break
+    case 3:
+        console.log("Miercoles");
+        break
+    case 4:
+        console.log("Jueves");
+        break
+    case 5:
+        console.log("Viernes");
+        break
+    case 6:
+        console.log("Sabado");
+        break
+    default:
+        break;
+}
