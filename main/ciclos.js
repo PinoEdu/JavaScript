6d7be7e80d27d8a07036804af74451588f393cec
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-08 20:02:11 -0300

    feat: break y continue

--- main/ciclos.js
+++ main/ciclos.js
@@ -17,3 +17,20 @@ do {
 for(let i=0; i < 10; i++){
     console.log("for",i);
 }   
+
+// ***Break & Continue***
+const numeros = [0,1,2,3,4,5,6,7,8,9];
+
+for (let index = 0; index < numeros.length; index++) {
+    if (index === 5){
+        break;          // Se va romper el ciclo cuando el index sea 5 
+    }
+    console.log(numeros[index]);
+}
+
+for (let index = 0; index < numeros.length; index++) {
+    if (index === 5){
+        continue;       // Se va saltar la iteracion cuando index sea 5
+    }
+    console.log(numeros[index]);
+}
