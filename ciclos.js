17a38baef0fb9c7cf737d7609d01b333bf6da32c
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-08 19:53:45 -0300

    feat: ciclos

---
+++ ciclos.js
@@ -0,0 +1,19 @@
+// ***Ciclos***
+let contador = 0;
+
+// Ciclo while
+while (contador < 10){
+    console.log("do",contador);
+    contador++;
+}
+
+// Ciclo while-do
+do {
+    console.log("while",contador);
+    contador++;
+} while (contador < 10);
+
+// Ciclo for
+for(let i=0; i < 10; i++){
+    console.log("for",i);
+}   
