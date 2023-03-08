d69e04840ebc1c0cd49ed327d2247fc9d82c34d6
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-07 22:29:00 -0300

    feat: tipos de operadores

---
+++ main/tipos_de_operadores.js
@@ -0,0 +1,25 @@
+// ***Operadores***
+
+// Aritmeticos
+let a = 5 + 5;
+let b = 5 * 5;
+let c = 5 / 5;
+let d = 5 % 2;
+
+// Relacionales
+console.log(8 > 9);
+console.log(8 < 9);
+console.log(8 >= 9);
+console.log(8 == 9);
+console.log(8 != 9);
+console.log("9" === 9); // Compara los valores y tipos de datos
+console.log("9" !== 9); //Compara los valores y sus tipos
+
+// Operadores de incremento o decremento
+let i = 1;
+i += 1;
+i *= 3;
+
+// Operadores unarios
+i++;
+i--;
