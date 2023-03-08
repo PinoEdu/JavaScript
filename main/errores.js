f1e106567a8adcf2a6df873274f5c84c37763ed2
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-08 19:56:29 -0300

    feat: manejo de errores

---
+++ main/errores.js
@@ -0,0 +1,9 @@
+// ***Manejo de errores***
+
+try{
+    console.log("En el try se agrega el código a evaluar");
+} catch (error) {
+    console.log("Catch, captura cualquier error surgido o lanzado en el try");
+} finally{
+    console.log("Se ejecutará al final de un try-catch");
+}
