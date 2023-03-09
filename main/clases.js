d9cadce0fa5bcee91ac145d467c2fb49ca38176d
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-08 21:12:22 -0300

    feat: clases y herencia de clases

---
+++ main/clases.js
@@ -0,0 +1,37 @@
+class Animal{
+    // Metodo especial para crear una clase
+    constructor(nombre, genero){
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+
+    // Metodos
+    saludar(){
+        console.log("Holaaaa");
+    }
+    despedir(){
+        console.log("Chao");
+    }
+}
+
+const mimi = new Animal("Mimi", "Hembra");
+const scooby = new Animal("Scooby", "Macho");
+
+class Perro extends Animal{
+    constructor(nombre, genero, tamanio){
+        // Metodo que llama al constructor de la clase padre
+        super(nombre,genero);
+        this.tamanio = tamanio;
+    }
+
+    // Metodos
+    saludar(){
+        console.log("Guau guau");
+    }
+    despedir(){
+        console.log("Guau");
+    }
+}
+
+const scooby2 = new Perro("Scooby", "Macho", "Grande");
+scooby2.saludar();
