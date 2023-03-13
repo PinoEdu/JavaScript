8033507c3e596be7b9b7131235fafe87b776f704
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-13 00:29:56 -0300

    feat: mapas

--- main/nuevos_tipos.js
+++ main/nuevos_tipos.js
@@ -42,3 +42,46 @@ for (item of set2){
 let arr = Array.from(set2);
 
 set2.clear();   // Limpiar el set completo
+
+
+// ***Maps***
+
+let mapa = new Map();
+
+mapa.set("nombre","Eduardo");
+mapa.set("apellido", "Pino");
+mapa.set("edad", 23);
+
+console.log(mapa);
+console.log(mapa.size);
+console.log(mapa.has("correo"));
+console.log(mapa.get("nombre"));
+
+mapa.set("nombre", "Edu");
+console.log(mapa);
+mapa.delete("apellido");
+
+for(let [key,value] of mapa){
+    console.log(`Llave: ${key}, Valor: ${value}`);
+}
+
+mapa.set(19, "diecinueve");
+mapa.set(false, "falso");
+mapa.set({},{});
+
+console.log(mapa);
+
+const mapa2 = new Map([
+    ["nombre", "Mauricio"],
+    ["edad", 23],
+    ["animal", "gato"],
+    [null, "nullo"]
+]);
+
+console.log(mapa2);
+
+const llavesMapa2 = [...mapa2.keys()];
+const valoresMapa2 = [...mapa2.values()];
+
+console.log(llavesMapa2);
+console.log(valoresMapa2);
