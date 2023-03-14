643c42e766c2cb266ed726de3f69e9d3bc2a4d69
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-14 14:10:27 -0300

    feat: generators

--- main/iterables.js
+++ main/iterables.js
@@ -14,3 +14,50 @@ while(!next.done){
     console.log(next.value);
     next = iterador.next();
 }
+
+// ***Generators***
+
+function* iterable1 (){
+    yield "hola 1";
+    console.log("Hola consola");
+    yield "hola 2";
+    console.log("Seguimos con más instrucciones de nuestro código");
+    yield "hola 3";
+    yield "hola 4";
+}
+
+let iterador2 = iterable1();
+
+for(let y of iterador2){
+    console.log(y);
+}
+
+const arr = [...iterable1()];
+console.log(arr);
+
+function cuadrado(valor){
+    setTimeout(() =>{
+        console.log({valor, resultado: valor*valor});
+    }, Math.random()*1000);
+
+    return{
+        valor,
+        resultado: valor*valor
+    }
+}
+
+function* generator(){
+    console.log("Inica generator");
+    yield cuadrado(0);
+    yield cuadrado(1);
+    yield cuadrado(2);
+    yield cuadrado(3);
+    yield cuadrado(4);
+    console.log("Termina generator");
+}
+
+let gen = generator();
+
+for(let y of gen){
+    console.log(y.value);
+}
