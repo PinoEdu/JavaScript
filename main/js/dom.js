2cb456795ac2110cee3d6ef7462764447d6e32f3
PinoEdu <eduardo.pinoh@usm.cl>
2023-03-31 00:29:12 -0300

    feat: bom

--- main/js/dom.js
+++ main/js/dom.js
@@ -109,3 +109,39 @@ $linkEventos.addEventListener("click", e => {
     alert(`Holaaaaaaaaaaaaaaaaaaaaaa`);
     e.preventDefault(); // Cancelar la accion que se tenga por default
 })
+
+// ***BOM: Propiedades y Eventos***
+window.addEventListener("resize", (e) =>{
+    console.clear();
+    console.log("***Evento Resize***");
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    console.log(window.outerHeight);
+    console.log(window.outerWidth);
+    console.log(window.scrollX);
+    console.log(window.scrollY);
+    console.log(e);
+})
+
+window.addEventListener("scroll", (e) =>{
+    console.clear();
+    console.log("***Evento Scroll***");
+    console.log(window.scrollX);
+    console.log(window.scrollY);
+    console.log(e);
+})
+
+window.addEventListener("load", (e) =>{
+
+    console.log("***Evento Load***");
+    console.log(window.scrollX);
+    console.log(window.scrollY);
+    console.log(e);
+})
+
+document.addEventListener("DOMContentLoaded", e => {
+    console.log("***Evento DOMContentLoaded***");
+    console.log(window.scrollX);
+    console.log(window.scrollY);
+    console.log(e);
+})
