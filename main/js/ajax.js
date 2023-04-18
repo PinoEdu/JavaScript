2c8abd537621d11e210ed2ff24a38504a796b08c
PinoEdu <eduardo.pinoh@usm.cl>
2023-04-17 22:14:02 -0400

    feat: API Fetch + Async-Await

--- main/js/ajax.js
+++ main/js/ajax.js
@@ -59,3 +59,36 @@
         console.log("Se ejecutará independiente de lo que ocurra previamente");
     });
 })();
+
+
+(() => {
+    const $fetchASync = document.getElementById("fetch-async");
+    const $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try{
+            let res = await fetch("https://jsonplaceholder.typicode.com/users");
+            let json = await res.json();
+             
+            console.log(res,json);
+            
+            if(!res.ok) throw {status: res.status, statusText: res.statusText}
+
+            json.forEach((el) => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $fetchASync.appendChild($fragment);
+        } catch(err){
+            console.log(err);
+            let message = err.statusText || "Ocurrio un error";
+            $fetchASync.innerHTML = `Error ${er.status}:${message}`;
+        } finally{
+            console.log("Se ejecutará igualmente")
+        }
+    }
+
+    getData();
+})();
